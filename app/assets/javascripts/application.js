a19d5386f9de65fe14869505014b0f59c20fd83f
lsdtc1225 <lsdtc1225@gmail.com>
2014-05-29 16:26:43 -0700

    rearrange require bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
-//= require bootstrap.min
+
 //= require turbolinks
 //= require_tree .
